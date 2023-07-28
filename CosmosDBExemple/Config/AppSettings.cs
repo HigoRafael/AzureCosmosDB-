e9722d0e431190f8e58cf5d00ba89e3c9042e1f4
Bowie <higorrafael390@gmail.com>
2023-07-28 03:56:20 -0400

    Adicionar arquivos de projeto.

---
+++ CosmosDBExemple/Config/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace CosmosDBExample.Config
+{
+    public static class AppSettings
+    {
+        public static readonly string CosmosDdEndpointUri = "https://firstdata.documents.azure.com:443/";
+        public static readonly string CosmosDdPrimaryKey = "jncvmpUPmOcEzANRo25CFbxyp9YO0GLPDyUov02IHtzHpSrgkV5UFHGfh2FMpxcCYzEPwaxsEf5KACDbHgSqyw==";
+    }
+}
