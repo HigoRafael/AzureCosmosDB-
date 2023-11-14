7c5c621c0e22954b47397cd2c890481175fe9a70
Higor Rafael <64501070+HigoRafael@users.noreply.github.com>
2023-11-14 12:25:07 -0300

    Update AppSettings.cs
    Remove Token de acesso

--- CosmosDBExemple/Config/AppSettings.cs
+++ CosmosDBExemple/Config/AppSettings.cs
@@ -3,6 +3,6 @@
     public static class AppSettings
     {
         public static readonly string CosmosDdEndpointUri = "https://firstdata.documents.azure.com:443/";
-        public static readonly string CosmosDdPrimaryKey = "jncvmpUPmOcEzANRo25CFbxyp9YO0GLPDyUov02IHtzHpSrgkV5UFHGfh2FMpxcCYzEPwaxsEf5KACDbHgSqyw==";
+        public static readonly string CosmosDdPrimaryKey = "";
     }
 }
